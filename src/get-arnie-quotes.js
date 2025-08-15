3a4e35f0816fa7e365de765fa8269955f58e3b90
Achira Shamal <jayasekaraachira@gmail.com>
2025-08-15 13:11:58 +1000

    Complete getArnieQuotes func to return quotes if success, fail messege if not

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,19 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+
+    const requests = urls.map(async (url) => {
+    const { status, body } = await httpGet(url);
+    const { message } = JSON.parse(body);
+
+    if (status === 200) {
+      return { 'Arnie Quote': message };
+    } else {
+      return { 'FAILURE': message };
+    }
+  });
+
+  return Promise.all(requests);
 };
 
 module.exports = {
